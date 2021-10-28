94673026d78e0d0b350a4ef842c763b037a2f488
Aziel Freitas <azielfreitas@gmail.com>
2021-10-28 16:29:35 -0300

    Add transparency to price columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,11 @@ def load_mapping_df():
 # Saves dataframe into a variable and drops rows containing '<NA>'
 # since every column has absolute priority.
 main_df = load_mapping_df()
-no_na_df = main_df.dropna()
+missing_coords = int(max(main_df['Latitude'].isna().sum(),
+                      main_df['Latitude'].isna().sum()))
+main_df.dropna(inplace=True)
+main_df.drop(main_df[main_df['Price'] > 2000000].index, inplace=True)
+# no_na_df.drop([no_na_df[no_na_df['Price'] > 2000000.0]].index, inplace=True)
 # IMPORTANT! FORMAT!!
 # st.write(no_na_df.style.format({'Price': "R$ {:,.2f}"}))
 
@@ -55,8 +59,7 @@ with st.expander('Dataframe information'):
     st.metric(label="Number or properties", value=len(main_df['Property_ID']))
   with c2:
     st.metric("Properties missing coordinates",
-              int(max(main_df['Latitude'].isna().sum(),
-                      main_df['Latitude'].isna().sum())))
+              missing_coords)
   with c3:
     avg = main_df['Price'].mean()
     st.metric('Average price', f"R$ {avg:,.2f}")
@@ -73,54 +76,58 @@ with st.expander('Dataframe information'):
   hist_classes = 40
   plt.grid(True)
   ax1.hist(prices, hist_classes, facecolor='darkcyan', alpha=0.5)
-  
-  # price_histogram, ax1 = plt.subplots()
-
   st.pyplot(price_histogram)
+  
 # Mean coordinates that are used to center the properties on the map  
-mean_longitude = no_na_df['Longitude'].mean().round(6)
-mean_latitude = no_na_df['Latitude'].mean().round(6)
+mean_longitude = main_df['Longitude'].mean().round(6)
+mean_latitude = main_df['Latitude'].mean().round(6)
+
+def price_to_color_coding():
+  price_min = min(main_df['Price'])-1
+  price_max = max(main_df['Price'])+1
+  price_step = (price_max-price_min)/7
+  print(price_min+price_step)
+  bins = [price_min, price_min+price_step, price_min+price_step*2,
+          price_min+price_step*3, price_min+price_step*4, price_min+price_step*5, price_min+price_step*6, np.inf]
+  print(bins)
+  colors = {"red": '(255,45,0)', "orange": '(255,165,0)', "yellow":'(245,255,0)',
+            "green": '(50,255,0)', "cyan": '(0,235,255)', "blue": '(0,10,255)',
+            "violet": '(230,0,255)'}
+  transparency = [255, 200, 150, 100, 75, 50, 25]
+  main_df['Color'] = pd.cut(main_df['Price'], bins, labels=colors)
+  main_df['Transparency'] = pd.cut(main_df['Price'], bins, labels=transparency)
+  main_df['Color'] = main_df['Color'].replace(colors)
+  c_red = []
+  c_green = []
+  c_blue = []
+  for each_color in list(main_df['Color']):
+    components = each_color.split(',')
+    c_red.append(components[0].split('(')[1])
+    c_green.append(components[1])
+    c_blue.append(components[2].split(')')[0])
+  main_df['Red'] = [int(x) for x in c_red]
+  main_df['Green'] = [int(x) for x in c_green]
+  main_df['Blue'] = [int(x) for x in c_blue]
+  main_df.drop('Color', axis=1, inplace=True)
+
+price_to_color_coding()
 
 # Begins a portion of code concerning the sidebar. Can be appended later on thanks to "with" usage.
 with st.sidebar:
   st.subheader('Price range slider')
-  filter_slider = st.slider('R$ x1.000', min(no_na_df['Price'])/1000,
-                            max(no_na_df['Price'])/1000,
-                            (min(no_na_df['Price'])/1000,
-                             max(no_na_df['Price'])/1000), step=10.0,
+  filter_slider = st.slider('R$ x1.000', min(main_df['Price'])/1000,
+                            max(main_df['Price'])/1000,
+                            (min(main_df['Price'])/1000,
+                             max(main_df['Price'])/1000), step=20.0,
                             key='filter_slider',
                             help='Maximum and minimum values were based on **.csv** data.')
   lowest_price = filter_slider[0]
   highest_price = filter_slider[1]
 
 # Limits the dataframe according to the Prices slider
-no_na_df = no_na_df[(no_na_df.Price >= lowest_price*1000) &
-              (no_na_df.Price <= highest_price*1000)]
+main_df = main_df[(main_df.Price >= lowest_price*1000) &
+              (main_df.Price <= highest_price*1000)]
 
-def price_to_color_coding():
-  bins = [200000, 320000, 440000, 580000, 700000, 820000, 1000000, np.inf]  
-  colors = {"red": '(255,45,0)', "orange": '(255,165,0)', "yellow":'(245,255,0)',
-            "green": '(50,255,0)', "cyan": '(0,235,255)', "blue": '(0,10,255)',
-            "violet": '(230,0,255)'}
-  no_na_df['Color'] = pd.cut(no_na_df['Price'],bins,labels=colors)
-  no_na_df['Color'] = no_na_df['Color'].replace(colors)
-  c_red = []
-  c_green = []
-  c_blue = []
-  for each_color in list(no_na_df['Color']):
-    components = each_color.split(',')
-    c_red.append(components[0].split('(')[1])
-    c_green.append(components[1])
-    c_blue.append(components[2].split(')')[0])
-  no_na_df['Red'] = [int(x) for x in c_red]
-  no_na_df['Green'] = [int(x) for x in c_green]
-  no_na_df['Blue'] = [int(x) for x in c_blue]
-  no_na_df.drop('Color', axis=1, inplace=True)
-  
-price_to_color_coding()
-
-def radius():
-  return 8
 
 with st.expander('City map', expanded=True):
 # Draws map
@@ -139,19 +146,19 @@ with st.expander('City map', expanded=True):
       layers=[
         pdk.Layer(
           'ColumnLayer',
-          data=no_na_df,
+          data=main_df,
           get_position='[Longitude, Latitude]',
           get_elevation='[Price]',
-          radius=radius(),
+          radius=6,
           elevation_scale=0.0005,
-          get_fill_color=['Red','Green','Blue',125],
+          get_fill_color=['Red','Green','Blue','Transparency'],
           auto_highlight=True,
           pickable=True,
         ),
       ],
       tooltip = {
       "html": "ID: <i>{Property_ID}</i><br/> R$ <b>{Price}</b>",
-      "style": {"background": "blue", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10"},
+      "style": {"background": "steelblue", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10"},
       }
     )
   )
